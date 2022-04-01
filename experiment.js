387623e48995db8a5ef7907f59bf45a34e5b43ac
Fedor <fedorchuvilkin.nl@gmail.com>
2022-04-01 16:49:08 +0200

    Initialize experiment with Analytics SDK

--- experiment.js
+++ experiment.js
@@ -4,14 +4,8 @@ import { Experiment } from '@amplitude/experiment-js-client';
 const apiKey = 'client-8zpzwcNEjp3DmsBT2LWd5S7jj6Y7zBqv';
 
 // (2) Initialize the experiment client
-const experiment = Experiment.initialize(apiKey);
+const experiment = Experiment.initializeWithAmplitudeAnalytics(apiKey);
 
 // (3) Fetch variants for a user
-const user = {
-  user_id: 'USER-ID',
-  device_id: 'DEVICE-ID',
-  user_properties: {
-    'custom': 'value',
-  },
-};
-await experiment.fetch(user);
+
+await experiment.fetch();
