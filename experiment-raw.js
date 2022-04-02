9526fbb7823b145a6ae6d666ed9d3a009e4ce202
Fedor <fedorchuvilkin.nl@gmail.com>
2022-04-02 18:46:38 +0200

    Implement and test Experiment

--- experiment-raw.js
+++ experiment-raw.js
@@ -1,3 +1,6 @@
+import amplitude from 'amplitude-js';
+
+
 import { Experiment } from '@amplitude/experiment-js-client';
 
 
@@ -5,6 +8,7 @@ function initializeExperiment() {
 // (1) Get your deployment's API key
 const apiKey = 'client-8zpzwcNEjp3DmsBT2LWd5S7jj6Y7zBqv';
 
+amplitude.getInstance().init("53d662ed165eae375ae4ade7767e238d");
 // (2) Initialize the experiment client
 const experiment = Experiment.initializeWithAmplitudeAnalytics(apiKey);
 
@@ -12,6 +16,27 @@ const experiment = Experiment.initializeWithAmplitudeAnalytics(apiKey);
 
 const expAssignment = experiment.fetch();
 
+return expAssignment;
+
 }
 
-const initExperiment = setTimeout(initializeExperiment, 450)
+const initExperiment = initializeExperiment()
+
+function showVariant() {
+  console.log(initExperiment)
+  initExperiment.then(experiment => {
+    console.log(experiment)
+    const variant = experiment.variant("background-color-experiment")
+    console.log(variant);
+    if (variant.value === 'treatment') {
+      // Flag is on
+      document.querySelector("body").classList.add("inverted");
+
+    } else {
+        // Flag is off
+}
+  });
+
+}
+
+showVariant();
